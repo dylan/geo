459f65067378967993a43c683a68cc08b237bd24
Dylan Wreggelsworth <dylan@bvr.io>
2017-01-22 17:16:48 -0500

    Initial Commit

---
+++ Geo/Geo.h
@@ -0,0 +1,19 @@
+//
+//  Geo.h
+//  Geo
+//
+//  Created by Dylan Wreggelsworth on 1/22/17.
+//  Copyright © 2017 BVR, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Geo.
+FOUNDATION_EXPORT double GeoVersionNumber;
+
+//! Project version string for Geo.
+FOUNDATION_EXPORT const unsigned char GeoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Geo/PublicHeader.h>
+
+
